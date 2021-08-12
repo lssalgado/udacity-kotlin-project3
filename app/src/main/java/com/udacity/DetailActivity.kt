1255e1516695b1ad444a7d8b6846b57c3caa5d3b
lsalgado <lsalgado@muxi.com.br>
2021-08-12 20:32:04 -0300

    Adding motion namespace and fixing transition start.

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -24,8 +24,6 @@ class DetailActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         handleIntent()
-
-        (motionLayout as MotionLayout).transitionToEnd()
     }
 
     private fun handleIntent() {
