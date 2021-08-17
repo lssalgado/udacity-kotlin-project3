69fc4a0dcbbc0f5163e32799ac3282390d249824
lsalgado <lsalgado@muxi.com.br>
2021-08-17 01:19:06 -0300

    Updating values based on the downloaded file.

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,11 +1,17 @@
 package com.udacity
 
+import android.app.DownloadManager
 import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.properties.Delegates
 
 const val FILE_NAME = "fileName"
 const val DOWNLOAD_STATUS = "downloadStatus"
@@ -17,6 +23,20 @@ class DetailActivity : AppCompatActivity() {
         getSystemService(
             NotificationManager::class.java
         )
+    }
+    private val downloadManager: DownloadManager by lazy {
+        getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+    }
+
+    private var fileDetails: FileDetails? by Delegates.observable<FileDetails?>(null) { p, old, new ->
+        new?.let {
+            fileId.text = new.id.toString()
+            mediaType.text = new.mediaType
+            size.text = new.size.toString()
+            lastModified.text = new.lastModified
+            uri.text = new.uri
+            localUri.text = new.localUri
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,9 +52,11 @@ class DetailActivity : AppCompatActivity() {
         var sFileName = getString(R.string.missing_file_name_key)
         var downloadText = getString(R.string.fail)
         var textColor = getColor(R.color.red)
+        var downloadId = -1L
         extras?.let {
             sFileName = it.getString(FILE_NAME, sFileName)
             val bDownloadStatus = it.getBoolean(DOWNLOAD_STATUS)
+            downloadId = it.getLong(DOWNLOAD_ID, -1)
             if (bDownloadStatus) {
                 downloadText = getString(R.string.success)
                 textColor = getColor(R.color.colorPrimaryDark)
@@ -47,6 +69,40 @@ class DetailActivity : AppCompatActivity() {
 
         okButton.setOnClickListener {
             MainActivity.start(this)
+        }
+
+        getFileInfo(downloadId)
+    }
+
+    private fun getFileInfo(id: Long) {
+        CoroutineScope(Dispatchers.Default).launch {
+            val query = DownloadManager.Query()
+            query.setFilterById(id)
+            val cursor = downloadManager.query(query)
+
+
+            if (cursor.moveToFirst()) {
+                val fileId = cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_ID))
+                val mediaType =
+                    cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE))
+                val size =
+                    cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+                val lastModifiedInt =
+                    cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_LAST_MODIFIED_TIMESTAMP))
+                val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                val lastModified = formatter.format(lastModifiedInt)
+                val uri = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_URI))
+                val localUri =
+                    cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
+                fileDetails = FileDetails(
+                    fileId,
+                    mediaType,
+                    size,
+                    lastModified,
+                    uri,
+                    localUri
+                )
+            }
         }
     }
 
